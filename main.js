8b2d24637a7723bf37cf673132d5590697887d17
pinniheo211 <84260434+pinniheo211@users.noreply.github.com>
2023-02-23 22:08:55 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var openButton = document.getElementById("menu_button");
+var menuMobile = document.getElementById("menu_mobile");
+
+
+openButton.addEventListener("click",function open(){
+    menuMobile.style.display = "block";
+});
+
+
+
